7d6ed8dcbf6cfc0ed0c9cb7496fa4d9a9950302b
cida <cida@netop.com>
2016-10-11 11:31:54 +0300

    add some docs to where the paths are

--- index.js
+++ index.js
@@ -2,12 +2,6 @@
 
 var express = require('express');
 var mock = express();
-if(true) {
-    mock.use(function (request, response, next) {
-        console.log("[" + request.method + "]   " + request.path);
-        next();
-    });
-}
 
 var topicRouter = require('./lib/routes/topics');
 var journalRouter = require('./lib/routes/journals');
@@ -17,4 +11,27 @@ mock.use(topicRouter.path, topicRouter);
 mock.use(journalRouter.path, journalRouter);
 
 
+
+mock.all(function (request, response) {
+   response.json({
+       topics: {
+           root: topicRouter.path,
+           apis: {
+               "/" : "GET",
+               "/:id": "GET",
+               "/:id/journals": "GET",
+               "/journals/:id": "GET"
+           }
+       },
+       entries: {
+           root: journalRouter.path,
+           apis: {
+               "/" : "GET",
+               "/:id": "GET",
+               "/download/:id": "GET",
+                "/:id/download": "GET"
+           }
+       }
+   });
+});
 module.exports = mock;
